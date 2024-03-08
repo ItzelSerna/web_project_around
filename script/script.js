527a118ce736ed927e89df8427fbf096a019c0f3
ITZEL CONCEPCIÓN SERNA GAVILANES <itzel.c.serna.g19@gmail.com>
2024-03-08 15:25:35 -0500

    feat: Agregar funcion ESC y overlay

--- script/script.js
+++ script/script.js
@@ -2,206 +2,215 @@
 
 const page = document.querySelector(".page");
 
-document.addEventListener("DOMContentLoaded", function () {
-  //Popup editar perfil
-  const profileNameElement = document.querySelector(".profile__name");
-  const profileAboutElement = document.querySelector(".profile__about");
+//Popup editar perfil
+const profileNameElement = document.querySelector(".profile__name");
+const profileAboutElement = document.querySelector(".profile__about");
 
-  const profileName = profileNameElement.textContent;
-  const profileAbout = profileAboutElement.textContent;
+const profileName = profileNameElement.textContent;
+const profileAbout = profileAboutElement.textContent;
 
-  const profileEditButton = document.querySelector(".profile__edit-button");
-  const editPopupElement = document.querySelector(".popup");
+const profileEditButton = document.querySelector(".profile__edit-button");
+const editPopupElement = document.querySelector(".popup");
 
-  const formElement = document.querySelector(".popup__form");
-  const nameInput = document.querySelector(".popup__input_name");
-  const aboutInput = document.querySelector(".popup__input_about");
-  const closeButtonPopup = document.querySelector(".popup__close-button");
+const formElement = document.querySelector(".popup__form");
+const nameInput = document.querySelector(".popup__input_name");
+const aboutInput = document.querySelector(".popup__input_about");
+const closeButtonPopup = document.querySelector(".popup__close-button");
 
-  //Popup agregar imagenes
-  const profileAddButton = document.querySelector(".profile__add-button");
-  const addImagePopupElement = document.querySelector("#add-image-popup");
-  const titleInput = document.querySelector(".popup__input_title");
-  const imageInput = document.querySelector("#popup__input_image");
+//Popup agregar imagenes
+const profileAddButton = document.querySelector(".profile__add-button");
+const addImagePopupElement = document.querySelector("#add-image-popup");
+const titleInput = document.querySelector(".popup__input_title");
+const imageInput = document.querySelector("#popup__input_image");
 
-  //Template cards
-  const templateCard = document.querySelector(".template-card");
-  const cardArea = document.querySelector(".cards");
-  const formCard = document.querySelector(".popup__form");
-  const initialCards = [
-    {
-      name: "Valle de Yosemite",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-    },
-    {
-      name: "Lago Louise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-    },
-    {
-      name: "Montañas Calvas",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
-    },
-    {
-      name: "Latemar",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-    },
-    {
-      name: "Parque Nacional de la Vanoise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-    },
-    {
-      name: "Lago di Braies",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-    },
-  ];
-  const showImage = document.querySelector("#popup-show-image");
-  const popupCloseImage = document.querySelector("#popup-close-image");
-  const popupImageClose = document.querySelector(".popup__image-close");
+//Template cards
+const templateCard = document.querySelector(".template-card");
+const cardArea = document.querySelector(".cards");
 
-  //Popup imagenes
-  const popupImageOpen = document.querySelector("#popup_image-open");
-  const closeImage = document.querySelector(".popup__close-button-image");
-  const popupImageTitle = document.querySelector(".popup__image-title");
-  const popupImage = document.querySelector(".popup__image");
+const groupImage = document.getElementById("add-image-popup");
+const formCard = groupImage.querySelector(".popup__form");
 
-  let initialProfileName = profileName;
-  let initialProfileAbout = profileAbout;
+const initialCards = [
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
+];
 
-  //Overlays
-  const overlayEdit = document.querySelector("#popup-overlay-edit");
-  const overlayAdd = document.querySelector("#popup-overlay-add");
-  const overlayImage = document.querySelector("#popup-overlay-image");
+//Popup imagenes
+const popupImageOpen = document.querySelector("#popup_image-open");
+const closeImage = document.querySelector("#popup__close-image");
+const popupImageTitle = document.querySelector(".popup__image-title");
+const popupImage = document.querySelector(".popup__image");
 
-  //Popup editar perfil
-  function setPopupInput() {
-    nameInput.value = initialProfileName;
-    aboutInput.value = initialProfileAbout;
-  }
+let initialProfileName = profileName;
+let initialProfileAbout = profileAbout;
 
-  function openPopup() {
-    editPopupElement.classList.add("popup_opened");
-    document.addEventListener("keydown", closeWithEsc);
-  }
+//Overlays
+const overlayEdit = document.querySelector("#popup-overlay-edit");
+const overlayAdd = document.querySelector("#popup-overlay-add");
+const overlayImage = document.querySelector("#popup-overlay-image");
 
-  function handlePopupClick(event) {
-    setPopupInput();
-    openPopup();
-  }
+//Popup editar perfil
+function setPopupInput() {
+  nameInput.value = initialProfileName;
+  aboutInput.value = initialProfileAbout;
+}
 
-  function closePopup() {
-    editPopupElement.classList.remove("popup_opened");
-    document.removeEventListener("keydown", closeWithEsc);
-  }
+function openPopup() {
+  editPopupElement.classList.add("popup_opened");
+  overlayEdit.addEventListener("click", handleOverlayClick);
+  document.addEventListener("keydown", closeWithEsc);
+}
 
-  function handleProfileFormSubmit(evt) {
-    evt.preventDefault();
-    profileNameElement.textContent = nameInput.value;
-    profileAboutElement.textContent = aboutInput.value;
-    initialProfileName = nameInput.value;
-    initialProfileAbout = aboutInput.value;
-    closePopup();
-  }
+function handlePopupClick(event) {
+  setPopupInput();
+  openPopup();
+}
 
-  //Popup agregar imagenes
-  function openAddImagePopup() {
-    addImagePopupElement.classList.add("popup_opened");
-  }
+function closePopup() {
+  editPopupElement.classList.remove("popup_opened");
+  overlayEdit.removeEventListener("click", handleOverlayClick);
+  document.removeEventListener("keydown", closeWithEsc);
+}
 
-  function handleAddImageClick(event) {
-    openAddImagePopup();
-  }
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = nameInput.value;
+  profileAboutElement.textContent = aboutInput.value;
+  initialProfileName = nameInput.value;
+  initialProfileAbout = aboutInput.value;
+  closePopup();
+}
 
-  function closeAddImagePopup() {
-    addImagePopupElement.classList.remove("popup_opened");
-  }
+//Popup agregar imagenes
+function openAddImagePopup() {
+  addImagePopupElement.classList.add("popup_opened");
+  overlayAdd.addEventListener("click", handleOverlayClick);
+  document.addEventListener("keydown", closeWithEsc);
+}
 
-  function handleAddImageFormSubmit(evt) {
-    evt.preventDefault();
-    const newImageTitle = titleInput.value;
-    const newImageUrl = imageInput.value;
-    closeAddImagePopup();
-  }
+function handleAddImageClick(event) {
+  openAddImagePopup();
+}
 
-  //Template cards
-  function cardGenerator(title, link) {
-    const card = templateCard.cloneNode(true).content.querySelector(".card");
-    const cardImage = card.querySelector(".card__photo");
-    const cardTitle = card.querySelector(".card__info-name");
-    const likeButton = card.querySelector(".card__like-button");
-    const deleteButton = card.querySelector(".card__delete-button");
-    cardImage.src = link;
-    cardTitle.textContent = title;
-    cardImage.alt = title;
-    likeButton.addEventListener("click", function () {
-      likeButton.classList.toggle("card__like-button_active");
-    });
-    deleteButton.addEventListener("click", function () {
-      card.remove();
-    });
-    cardImage.addEventListener("click", function () {
-      handleOpenImage(title, link);
-    });
-    return card;
-  }
+function closeAddImagePopup() {
+  addImagePopupElement.classList.remove("popup_opened");
+  overlayAdd.removeEventListener("click", handleOverlayClick);
+  document.removeEventListener("keydown", closeWithEsc);
+}
 
-  initialCards.forEach(function (element) {
-    const newCard = cardGenerator(element.name, element.link);
-    cardArea.append(newCard);
+function handleAddImageFormSubmit(evt) {
+  evt.preventDefault();
+  const newImageTitle = titleInput.value;
+  const newImageUrl = imageInput.value;
+  closeAddImagePopup();
+}
+
+//Template cards
+function cardGenerator(title, link) {
+  const card = templateCard.cloneNode(true).content.querySelector(".card");
+  const cardImage = card.querySelector(".card__photo");
+  const cardTitle = card.querySelector(".card__info-name");
+  const likeButton = card.querySelector(".card__like-button");
+  const deleteButton = card.querySelector(".card__delete-button");
+  cardImage.src = link;
+  cardTitle.textContent = title;
+  cardImage.alt = title;
+  likeButton.addEventListener("click", function () {
+    likeButton.classList.toggle("card__like-button_active");
   });
+  deleteButton.addEventListener("click", function () {
+    card.remove();
+  });
+  cardImage.addEventListener("click", function () {
+    handleOpenImage(title, link);
+  });
+  return card;
+}
 
-  function handleAddCardSubmit(evt) {
-    evt.preventDefault();
+initialCards.forEach(function (element) {
+  const newCard = cardGenerator(element.name, element.link);
+  cardArea.append(newCard);
+});
+
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  if (evt.submitter.classList.contains("popup__save-button")) {
     const newCard = cardGenerator(titleInput.value, imageInput.value);
     cardArea.prepend(newCard);
-    closeAddImagePopup();
   }
+  closeAddImagePopup();
+}
 
-  //Popup imagenes
-  function handleOpenImage(title, link) {
-    popupImage.src = link;
-    popupImageTitle.textContent = title;
-    popupImageOpen.classList.add("popup_opened");
-    closeImage.addEventListener("click", handleCloseImage);
-  }
-
-  function handleCloseImage() {
-    popupImageOpen.classList.remove("popup_opened");
-  }
-
-  //Overlays
-  function handleOverlayClick(event) {
-    console.log("Overlay clicked");
-    if (event.target === overlayEdit) {
-      closePopup();
-    } else if (event.target === overlayAdd) {
-      closeAddImagePopup();
-    } else if (event.target === overlayImage) {
-      handleCloseImage();
-    }
-  }
-
-  function closeWithEsc(event) {
-    if (event.key === "Escape") {
-      closePopup();
-    }
-  }
-
-  //Eventos abrir y cerrar
-  profileEditButton.addEventListener("click", handlePopupClick);
-  formElement.addEventListener("submit", handleProfileFormSubmit);
-
-  profileAddButton.addEventListener("click", handleAddImageClick);
-  formCard.addEventListener("submit", handleAddCardSubmit);
-
-  addImagePopupElement
-    .querySelector(".popup__close-button")
-    .addEventListener("click", closeAddImagePopup);
-  addImagePopupElement
-    .querySelector(".popup__form")
-    .addEventListener("submit", handleAddImageFormSubmit);
-
-  // Event listeners for overlay clicks
-  overlayEdit.addEventListener("click", handleOverlayClick);
-  overlayAdd.addEventListener("click", handleOverlayClick);
+//Popup imagenes
+function handleOpenImage(title, link) {
+  popupImage.src = link;
+  popupImageTitle.textContent = title;
+  popupImageOpen.classList.add("popup_opened");
+  closeImage.addEventListener("click", handleCloseImage);
   overlayImage.addEventListener("click", handleOverlayClick);
-});
+}
+
+function handleCloseImage() {
+  popupImageOpen.classList.remove("popup_opened");
+  overlayImage.removeEventListener("click", handleOverlayClick);
+}
+
+//Overlays
+function handleOverlayClick(event) {
+  if (event.target.classList.contains(".popup__overlay")) {
+    closePopup();
+    closeAddImagePopup();
+    handleCloseImage();
+  }
+}
+
+function closeWithEsc(event) {
+  console.log("closeWithEsc called");
+  if (event.key === "Escape") {
+    closePopup();
+    closeAddImagePopup(); // Close add image popup as well
+    handleCloseImage(); // Close image popup as well
+  }
+}
+
+//Eventos abrir y cerrar
+profileEditButton.addEventListener("click", handlePopupClick);
+formElement.addEventListener("submit", handleProfileFormSubmit);
+
+profileAddButton.addEventListener("click", handleAddImageClick);
+formCard.addEventListener("submit", handleAddCardSubmit);
+
+addImagePopupElement
+  .querySelector(".popup__close-button")
+  .addEventListener("click", closeAddImagePopup);
+addImagePopupElement
+  .querySelector(".popup__form")
+  .addEventListener("submit", handleAddImageFormSubmit);
+
+// Event listeners for overlay clicks
+overlayEdit.addEventListener("click", handleOverlayClick);
+overlayAdd.addEventListener("click", handleOverlayClick);
+overlayImage.addEventListener("click", handleOverlayClick);
+document.addEventListener("keydown", closeWithEsc);
